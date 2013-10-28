21b1c10c30609e4faa797d5e34ddef8231b9dbf6
Yuki Inoue <inouetakahiroki@gmail.com>
2013-10-29 08:39:57 +0900

    on work

--- Sqlite3.hpp
+++ Sqlite3.hpp
@@ -1,5 +1,5 @@
 
-#include <sqlite3>
+#include <sqlite3.h>
 #include <memory>
 
 
@@ -16,7 +16,7 @@ namespace sqlite3 {
 			 { sqlite3_close_v2(p_del); });
 	}
 
-	std::unique_ptr<sqlite3_stmt> prepare(std::unique_ptr<sqlite3> db, const char *sql) {
+	std::unique_ptr<sqlite3_stmt> prepare(const std::unique_ptr<sqlite3> &db, const char *sql) {
 		sqlite3_stmt *stmt;
 		sqlite3_prepare_v2
 			(db.get(),
@@ -26,8 +26,20 @@ namespace sqlite3 {
 			 nullptr);
 
 		return
-		ptr.reset(stmt, [](sqlite3_stmt *stmt_del){
+			std::unique_ptr<sqlite3_stmt>
+			(stmt, [](sqlite3_stmt *stmt_del){
 				sqlite3_finalize(stmt_del);
 			});
 	}
+
+	int exec(const std::unique_ptr<sqlite3> &db,
+	         const char *sql,
+	         int (*callback)(...) = nullptr,
+	         void *data = nullptr,
+	         char **errmsg = nullptr) {
+
+		return
+			sqlite3_exec(db.get(), sql, callback, data, errmsg);
+	}
+
 }
--- sm1.hh
+++ sm1.hh
@@ -3,10 +3,20 @@
 static int intervals[] = {0,1,6};
 
 
+// assumes EF, I, crct as columns in table `items'
+// nexpdate
+
+namespace CIndex {
+	enum {rowid, EF, I, crct, nexpdate};
+}
+
+
+
 struct ItemBase {
-	double EF = 2.5;
-	int crct = 0;
-	int I = 0;
+	void make(const std::unique_ptr<sqlite3> &db) {
+		sqlite3::exec(db, "insert into items(EF,I,crct) values(2.5, 0, 0)");
+	}
+
 
 	int nextI() const {
 		if (crct < 3)
@@ -25,8 +35,31 @@ struct ItemBase {
 	*/
 	virtual int getResponce() const = 0;
 
+	void test(const std::unique_ptr<sqlite3> &db) {
+		std::unique_ptr<sqlite3_stmt> stmt =
+			sqlite3::prepare
+			(db, "select rowid from items where nexpdate <= date('now')");
+
+		std::vector<int> rowids;
+		while(sqlite3_step(stmt.get()) == SQLITE_ROW)
+			rowids.push_back(sqlite3_column_int(stmt.get(),0));
+
+		std::vector<int> retest_ids;
+		for (int rowid : rowids)
+			if(test(db, rowid))
+				retest_ids.push_back(rowid);
+
+		while(!retest_ids.empty()) {
+			std::vector<int> result_ids;
+			for (int rowid : result_ids)
+				if (retest(rowid))
+					result_ids.push_back(rowid);
+			retest_ids.swap(result_ids);
+		}
+	}
+
 	// returns whether re-testing is needed.
-	bool test() {
+	bool test(const std::unique_ptr<sqlite3> &db, int rowid) {
 		int q = getResponce();
 		EF = EF-0.8+0.28*q-0.02*q*q;
 		crct++;
